7738d1ca68f36f5a1310f8441e1edb9dc04586e3
Phil <phil@hengetech.com.tw>
2023-10-26 17:56:42 +0800

    add one2one relationship

--- grails-app/domain/com/henge/User.groovy
+++ grails-app/domain/com/henge/User.groovy
@@ -7,9 +7,11 @@ class User {
     String homepage
     Date dateCreated
 
+    static hasOne = [profile: Profile]
     static constraints = {
         loginId size: 3..20, unique: true, nullable: false, matches: /[0-9]{7}[A-Za-z]*/
         password size: 6..20, nullable: false, validator: { password, user -> user.loginId != password }
         homepage url: true, nulllable: true
+        profile nullable: true
     }
 }
